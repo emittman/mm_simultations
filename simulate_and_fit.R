ad857fc3630785aaccd9fda1b7612084cdffcacf
emittman <emittman@iastate.edu>
2017-05-10 17:52:03 -0500

    Modify simulation and run

--- simulate_and_fit.R
+++ simulate_and_fit.R
@@ -14,39 +14,48 @@ sim_data <- function(G, X, group_n, mu_beta, sd_beta, corr=NULL, mu_dispersion, 
   }
   dispersion <- rlnorm(G, mu_dispersion, sd_dispersion)
   y <- t(sapply(1:G, function(g) rnbinom(N, mu = exp(X %*% beta[,g]), size = 1/dispersion[g])))
-  out <- list(formatData(y, X), truth = list(beta = beta))
+  out <- list(formatData(y, X), raw_counts = y, truth = beta)
   out
 }  
 
-G <- 1000
-X <- diag(3)
+G <- 10000
+X <- matrix(c(1, -.5, 0,
+              1, .5, 0,
+              1, 0, 1), 3, 3, byrow=T)
 
-m <- matrix(c(3, 1, 1,
+m <- matrix(c(3, 1, -.5,
               1, 2, .5,
-              1, .5, 1), 3, 3)
+              -.5, .5, 1), 3, 3)
 m
 
-prior_mean <- c(0,0,0)
+prior_mean <- c(3,0,0)
 
 cr <- cov2cor(m)
 prior_sd <- sqrt(diag(m))
 d <- sim_data(G, X, c(3,2,2), prior_mean, prior_sd, cr, -2, .5)
 d
 
-K <- 1000
+K <- 3000
 
 estimates <- indEstimates(d[[1]])
-priors <- formatPriors(K, prior_mean, prior_sd)
+prior_from_est <- informPriors(estimates)
+saveRDS(estimates, "estimates.rds")
+priors <- formatPriors(K = K, 
+                       prior_mean = prior_mean,
+                       prior_sd = prior_sd,
+                       a = prior_from_est$a,
+                       b = prior_from_est$b,
+                       A=1, B=.1) #vague prior on alpha
 #chain <- initChain(priors, G, estimates)
 system.time(
-s <- mcmc(d[[1]], priors, methodPi = "stickBreaking", n_iter=50000, idx_save=c(0,499,999), thin=1,
+s <- mcmc(d[[1]], priors, methodPi = "stickBreaking", n_iter=100000, idx_save=c(0,499,999), thin=1,
           n_save_P=100, alpha_fixed=F, verbose = 0, warmup=10000, slice_width=1, max_steps=100, estimates=estimates)
 )
 saveRDS(s, "samples_stick.rds")
 saveRDS(d[[2]], "truth.rds")
 
 system.time(
-s2 <- mcmc(d[[1]], priors, methodPi = "symmDirichlet", n_iter=50000, idx_save=c(0,499,999), thin=1,
+s2 <- mcmc(d[[1]], priors, methodPi = "symmDirichlet", n_iter=100000, idx_save=c(0,499,999), thin=1,
           n_save_P=100, alpha_fixed=F, verbose = 0, warmup=10000, slice_width=1, max_steps=100, estimates=estimates)
 )
 
