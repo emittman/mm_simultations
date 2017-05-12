969c9d4e3e8775ed7db934f7ef525986465072b3
emittman <emittman@iastate.edu>
2017-05-12 13:56:30 -0500

    Increase warmup, make reproducible

--- simulate_and_fit.R
+++ simulate_and_fit.R
@@ -1,5 +1,7 @@
 library(cudarpackage)
 
+set.seed(134312517)
+
 #simulate data
 sim_data <- function(G, X, group_n, mu_beta, sd_beta, corr=NULL, mu_dispersion, sd_dispersion){
   V <- length(group_n)
@@ -32,31 +34,35 @@ prior_mean <- c(3,0,0)
 
 cr <- cov2cor(m)
 prior_sd <- sqrt(diag(m))
-d <- sim_data(G, X, c(3,2,2), prior_mean, prior_sd, cr, -2, .5)
+d <- sim_data(G, X, c(4,4,6), prior_mean, prior_sd, cr, -2, .5)
 d
 
 K <- 3000
 
 estimates <- indEstimates(d[[1]])
-prior_from_est <- informPriors(estimates)
+counts <- d[[2]]
+truth <- d[[3]]
 saveRDS(estimates, "estimates.rds")
+saveRDS(counts, "counts.rds")
+saveRDS(truth, "truth.rds")
+
+prior_from_est <- informPriors(estimates)
 priors <- formatPriors(K = K, 
                        prior_mean = prior_mean,
                        prior_sd = prior_sd,
                        a = prior_from_est$a,
                        b = prior_from_est$b,
-                       A=1, B=.1) #vague prior on alpha
+                       A=5, B=.5) #vague prior on alpha
 #chain <- initChain(priors, G, estimates)
 system.time(
 s <- mcmc(d[[1]], priors, methodPi = "stickBreaking", n_iter=100000, idx_save=c(0,499,999), thin=1,
-          n_save_P=100, alpha_fixed=F, verbose = 0, warmup=10000, slice_width=1, max_steps=100, estimates=estimates)
+          n_save_P=100, alpha_fixed=F, verbose = 0, warmup=30000, estimates=estimates)
 )
 saveRDS(s, "samples_stick.rds")
-saveRDS(d[[2]], "truth.rds")
 
 system.time(
 s2 <- mcmc(d[[1]], priors, methodPi = "symmDirichlet", n_iter=100000, idx_save=c(0,499,999), thin=1,
-          n_save_P=100, alpha_fixed=F, verbose = 0, warmup=10000, slice_width=1, max_steps=100, estimates=estimates)
+          n_save_P=100, alpha_fixed=F, verbose = 0, warmup=30000, slice_width=1, max_steps=100, estimates=estimates)
 )
 
 saveRDS(s2, "samples_SD.rds")
